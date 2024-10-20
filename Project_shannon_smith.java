b8432bf5134c47128fe96a6f84ef7cd47f6d8c44
SchmitCodes <schmit.personal@gmail.com>
2024-10-20 13:29:53 -0400

    Added code to display all policy information the number of smokers, and the number of non-smokers
    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project_shannon_smith.java
+++ Project_shannon_smith.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.*;
 
 class Project_shannon_smith {
     /*
@@ -22,43 +24,83 @@ class Project_shannon_smith {
         }
         return fee;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // Array to hold policy objects
+        ArrayList<Policy> polices = new ArrayList<Policy>();
+        // Read the file and create new objs
+        //File file = new File("Policyinformation.txt");
+        File file = new File("Policyinformation.txt"); // Specify the path to your file here
 
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Please enter the Policy Number: ");
-        int policyNumber = keyboard.nextInt();
-        keyboard.nextLine();
-        System.out.print("Please enter the Provider Name: ");
-        String providerName = keyboard.nextLine();
-        System.out.print("Please enter the Policyholder's First Name: ");
-        String firstName = keyboard.nextLine();
-        System.out.print("Please enter the Policyholder's Last Name: ");
-        String lastName = keyboard.next();
-        System.out.print("Please enter the Policyholders Age: ");
-        int age = keyboard.nextInt();
-        System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-        String smokingStatus = keyboard.next();
-        System.out.print("Please enter the Policyholder's Height (in inches): ");
-        float height = keyboard.nextFloat();
-        System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-        float weight = keyboard.nextFloat();
-        keyboard.close();
-        Policy policyObj = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
-        double bmi = policyObj.calcBmi();
-        float policyPrice = policyPrice(policyObj.age, policyObj.smokingStatus, bmi);
-        System.out.printf("\nPolicy Number: %s\n" + 
-        "Provider Name: %s\n" + 
-        "Policyholder's First Name: %s\n" + 
-        "Policyholder's Last Name: %s\n" + 
-        "Policyholder's Age: %d\n" + 
-        "Policyholder's Smoking Status: %s\n" + 
-        "Policyholder's Height: %.2f\n" + 
-        "Policyholder's Weight: %.2f\n" + 
-        "Policyholder's BMI: %.2f\n" + 
-        "Policy Price: $%.2f\n", 
-        policyObj.policyNumber, policyObj.providerName, policyObj.firstName, 
-        policyObj.lastName, policyObj.age, policyObj.smokingStatus, 
-        policyObj.height, policyObj.weight, bmi, policyPrice);
+        Scanner inputFile = new Scanner(file);
 
-    }
-}
+        String policyNumberString;
+        int policyNumber;
+        String providerName;
+        String firstName;
+        String lastName;
+        String ageString;
+        int age;
+        String smokingStatus;
+        String heightString;
+        float height;
+        String weightString; 
+        float weight;
+
+        while(inputFile.hasNext()) {
+            // First line
+            policyNumberString = inputFile.nextLine();
+            policyNumber = Integer.parseInt(policyNumberString);
+            providerName = inputFile.nextLine();
+            firstName = inputFile.nextLine();
+            lastName = inputFile.nextLine();
+            ageString = inputFile.nextLine();
+            age = Integer.parseInt(ageString);
+            smokingStatus = inputFile.nextLine();
+            heightString = inputFile.nextLine();
+            height = Float.parseFloat(heightString);
+            weightString = inputFile.nextLine();   
+            weight = Float.parseFloat(weightString);
+            if(inputFile.hasNext())
+            { 
+                System.out.print("\n");   
+               inputFile.nextLine();//skip the blank line if we have not reached the end of the file
+            }
+            Policy policyObj = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
+            polices.add(policyObj);
+        }
+        inputFile.close();
+        int isSmoker = 0;
+        for (Policy p : polices) {
+            policyNumber = p.getPolicyNumber();
+            providerName = p.getProviderName();
+            String providerFirstName = p.getFirstName();
+            String providerLastName = p.getLastName();
+            age = p.getAge();
+            smokingStatus = p.getSmokingStatus();
+            if (smokingStatus.equals("smoker")) {isSmoker++;}
+            height = p.getHeight();
+            weight = p.getWeight();
+            double bmi = p.calcBmi();
+            float pPrice = policyPrice(age, smokingStatus, bmi);
+
+            System.out.printf("\nPolicy Number: %s\n" + 
+            "Provider Name: %s\n" + 
+            "Policyholder's First Name: %s\n" + 
+            "Policyholder's Last Name: %s\n" + 
+            "Policyholder's Age: %d\n" + 
+            "Policyholder's Smoking Status: %s\n" + 
+            "Policyholder's Height: %.2f\n" + 
+            "Policyholder's Weight: %.2f\n" + 
+            "Policyholder's BMI: %.2f\n" + 
+            "Policy Price: $%.2f\n", 
+            policyNumber, providerName, providerFirstName, 
+            providerLastName, age, smokingStatus, 
+            height, weight, bmi, pPrice);
+            } // End for loop for display
+            int nonSmoker = polices.size() - isSmoker;
+            System.out.printf("\nThe number of policies with a smoker is: %d", isSmoker);
+            System.out.printf("\nThe number of policies with a non-smoker is: %d", nonSmoker);
+
+
+    } // end main
+} // end class
